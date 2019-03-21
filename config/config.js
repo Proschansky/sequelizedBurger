951583fd2464aff47bc282e67da4f14cdebff3e4
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-03-20 20:16:59 -0400

    Adjusted config

--- config/config.js
+++ config/config.js
@@ -17,10 +17,11 @@ module.exports = {
     "dialect": "mysql"
 },
 "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  "username": process.env.USER,
+  "password": process.env.PASSWORD,
+  "database": process.env.DATABASE,
+  "host": process.env.HOST,
+  "port": 3306,
+  "dialect": "mysql"
 }
 }
