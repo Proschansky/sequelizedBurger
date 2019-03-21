281cfb47af8f4274dc84d6a98cade6eaf8150478
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-03-20 20:20:19 -0400

    Changed config for Heroku

--- config/config.js
+++ config/config.js
@@ -10,12 +10,13 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-},
+      "username": process.env.USER,
+      "password": process.env.PASSWORD,
+      "database": process.env.DATABASE,
+      "host": process.env.HOST,
+      "port": 3306,
+      "dialect": "mysql"
+    },
 "production": {
   "username": process.env.USER,
   "password": process.env.PASSWORD,
